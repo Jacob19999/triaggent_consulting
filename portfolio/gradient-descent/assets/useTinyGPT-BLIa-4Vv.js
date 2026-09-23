99d91d8ee13d556db5882d31d824eb6ee38578ce
Jacob Tang <tngzjn54@gmail.com>
2026-09-23 16:20:43 -0500

    Add Jacob Tang course credit to Gradient Descent page

---
+++ portfolio/gradient-descent/assets/useTinyGPT-BLIa-4Vv.js
@@ -0,0 +1,1 @@
+import{D as e,a as t}from"./index-CXV9PaiV.js";var n=e();function r(){let[e,r]=(0,n.useState)(null);return(0,n.useEffect)(()=>{let e=!0;return t().then(t=>e&&r(t)),()=>{e=!1}},[]),e}export{r as t};
